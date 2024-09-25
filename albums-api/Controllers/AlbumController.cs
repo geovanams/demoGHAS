0f00ff2cfd905cf88c350649e65651af03820450
Geovana Martins <geeovanamartins@outlook.com>
2024-09-25 03:23:09 -0300

    first

---
+++ albums-api/Controllers/AlbumController.cs
@@ -0,0 +1,39 @@
+﻿using albums_api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+using System.Text;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace albums_api.Controllers
+
+public const string BASKET_COOKIENAME = "eShop";
+public const int ITEMS_PER_PAGE = 10;
+public const string DEFAULT_USERNAME = "Guest";
+public const string BASKET_ID = "BasketId";
+public const string STORAGE_TEST = "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"; 
+
+{
+    [Route("albums")]
+    [ApiController]
+    public class AlbumController : ControllerBase
+    {
+        // GET: api/album
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var albums = Album.GetAll();
+
+            return Ok(albums);
+        }
+
+        // GET api/<AlbumController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ albums-api/Controllers/UnsecuredController.cs
@@ -0,0 +1,80 @@
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace UnsecureApp.Controllers
+{
+    public class MyController
+    {
+
+        public string ReadFile(string userInput)
+        {
+            using (FileStream fs = File.Open(userInput, FileMode.Open))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    return temp.GetString(b);
+                }
+            }
+
+            return null;
+        }
+
+        public int GetProduct(string productName)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand()
+                {
+                    CommandText = "SELECT ProductId FROM Products WHERE ProductName = @productName",
+                    CommandType = CommandType.Text,
+                };
+                sqlCommand.Parameters.AddWithValue("@productName", productName);
+
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                return reader.GetInt32(0); 
+            }
+        }
+
+        public int GetProductt(string productName)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand()
+                {
+                    CommandText = "SELECT ProductId FROM Products WHERE ProductName = @productName",
+                    CommandType = CommandType.Text,
+                };
+                sqlCommand.Parameters.AddWithValue("@productName", productName);
+
+                connection.Open();
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+                if (reader.Read())
+                {
+                    return reader.GetInt32(0);
+                }
+                return -1; // or handle the case where no product is found
+            }
+        }
+
+        public void GetObject()
+        {
+            try
+            {
+                object o = null;
+                o.ToString();
+            }
+            catch (Exception e)
+            {
+                this.Response.Write(e.ToString());
+            }
+        
+        }
+
+        private string connectionString = "";
+    }
+}
